7a2d29c146680e7bd344db7b6a17d527c8e42a73
HGMDeveloperAndroid <136506541+HGMDeveloperAndroid@users.noreply.github.com>
2023-06-28 20:53:26 -0600

    Update ReportingController.php
    consulta

--- src/app/Http/Controllers/ReportingController.php
+++ src/app/Http/Controllers/ReportingController.php
@@ -369,12 +369,12 @@ class ReportingController extends Controller
         $pages = 50;
         if ($request->filled('perPage')) {
             $pages = $request->perPage;
-        }
-        $ranking = $this->reportingRepository->rankingEfficiency($request);*/
+        }*/
+        $ranking = $this->reportingRepository->rankingEfficiency($request);
         //$ranking = null;
         //$collection = new RankingEfficiencyCollection($ranking->paginate($pages));
-        $collection = 'hola mundo';
-        //$collection = $ranking;
+        //$collection = 'hola mundo';
+        $collection = $ranking;
         return $collection;
     }
 
